b7b458aeded13f42e821787b08b1633d33fcf031
fuyther <esiredrise@gmail.com>
2019-12-20 21:41:26 +0600

    saving preferences started

--- app/src/main/java/com/team2adevs/izho/MyApplication.java
+++ app/src/main/java/com/team2adevs/izho/MyApplication.java
@@ -1,6 +1,7 @@
 package com.team2adevs.izho;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.text.format.DateFormat;
 
 import java.sql.Timestamp;
@@ -9,18 +10,73 @@ import java.util.Date;
 import java.util.List;
 
 public class MyApplication extends Application {
-    private ArrayList<Integer> ids = new ArrayList<>();
-    private String day = "";
-    private String type = "st";
+    private ArrayList<Integer> ids = reloadIds();
+    private String day = reloadDay();
+    private String type = reloadType();
+    private SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+    private SharedPreferences.Editor editor = sharedPreferences.edit();
 
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String ID = "id";
+    public static final String DAY = "day";
+    public static final String TYPE = "type";
+
+    private void updateIds(ArrayList ids){
+        editor.putString(TYPE, type);
+        for(int i = 0; i < ids.size(); i++){
+            editor.putInt(ID + (i + 1), (int)ids.get(i));
+        }
+        editor.apply();
+    }
+
+    private void updateDay(String day){
+        editor.putString(DAY, day);
+        editor.apply();
+    }
+
+    private void updateType(String type){
+        editor.putString(TYPE, type);
+        editor.apply();
+    }
+
+    private String reloadDay(){
+        try{
+            return sharedPreferences.getString(DAY, "");
+        } catch (Exception e){
+            return "";
+        }
+    }
+
+    private String reloadType(){
+        try{
+            return sharedPreferences.getString(TYPE, "st");
+        } catch (Exception e){
+            return "st";
+        }
+    }
+
+    private ArrayList<Integer> reloadIds(){
+        ArrayList<Integer> ids = new ArrayList<>();
+        int counter = 1;
+        while(true){
+            int id = sharedPreferences.getInt(ID + counter, -1);
+            if (id == -1){
+                break;
+            }
+            ids.add(id);
+        }
+        return ids;
+    }
 
     public void append(int id){
         System.out.println(id);
         ids.add(id);
+        updateIds(ids);
     }
 
     public void setDay(String day){
         this.day = day;
+        updateDay(day);
     }
 
     public String getDay(){
@@ -35,7 +91,10 @@ public class MyApplication extends Application {
         return ids;
     }
 
-    public void setType(String type) { this.type = type; }
+    public void setType(String type) {
+        this.type = type;
+        updateType(type);
+    }
 
     public String getType() { return type; }
 
